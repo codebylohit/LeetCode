23700ab67e12ad3237d7bff70ecdd5750e7f2ac1
codebylohit <codelohit@gmail.com>
2025-06-25 00:49:32 +0530

    Add solution for LeetCode 771 - Jewels and Stones in Python

---
+++ Python/771_Jewels_and_Stones.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        sum = 0
+        for i in jewels:
+            if i in stones:
+                sum+=stones.count(i)
+        return sum
