c6b92d87ee9679da9b986c5bef5908755323676d
codebylohit <codelohit@gmail.com>
2025-06-26 23:54:27 +0530

    Add solution for LeetCode 2894 - Divisible and Non-divisible Sums Difference in Python

---
+++ Python/2894_Divisible_and_Non-divisible_Sums_Difference.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        num1 = 0
+        num2 = 0
+        for i in range(n+1):
+            if i%m:
+                num1+=i
+            else:
+                num2 +=i
+        return num1-num2
