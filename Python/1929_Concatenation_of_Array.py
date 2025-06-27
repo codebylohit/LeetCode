6e31c520b22f483b40189a8a82983a109542917c
codebylohit <codelohit@gmail.com>
2025-06-27 17:34:49 +0530

    Add solution for LeetCode 1929  - Concatenation of Array in Python

---
+++ Python/1929_Concatenation_of_Array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        '''length = len(nums)
+        for i in range(length):
+            nums.append(nums[i])
+        return nums'''
+
+        return nums+nums
+
