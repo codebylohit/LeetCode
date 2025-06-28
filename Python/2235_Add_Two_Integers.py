f10003055d422d31b0ab474ff6e99c78c8368dda
codebylohit <codelohit@gmail.com>
2025-06-28 21:33:18 +0530

    Add solution for LeetCode 2235 - Add Two Integers in Python

---
+++ Python/2235_Add_Two_Integers.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        if -100 <= num1 and num2 <= 100:
+            return num1+num2
+        
